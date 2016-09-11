87544d621c04cfb9f38f2a92f25dec055cf31c10
guitarhyo <guitarhyo@gmail.com>
2016-09-11 23:34:53 +0900

    페이스북 사진 공유 추가
    일단 재원이 소스에 추가 하였습니다.

---
+++ YuJaTest/app/src/main/java/map/test/yuja/example/jaewon/yujatest/SignInActivity.java
@@ -0,0 +1,124 @@
+package map.test.yuja.example.jaewon.yujatest;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.facebook.login.LoginManager;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
+
+public class SignInActivity extends AppCompatActivity {
+    ShareDialog shareDialog;
+    private SurfaceHolder mHolder;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        Button b = (Button)findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginManager.getInstance().logOut();
+
+            }
+        });
+
+        shareDialog = new ShareDialog(this);
+
+
+
+        Button btn2 = (Button) findViewById(R.id.button4); //xml 버튼을 찾습니다.
+        //버튼 클릭 메소드를 생성 합니다.
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+               ShareLinkContent content = new ShareLinkContent.Builder()
+                        .setContentUrl(Uri.parse("https://developers.facebook.com"))
+                        .setContentTitle("test")
+                        .setImageUrl(Uri.parse("https://static.xx.fbcdn.net/rsrc.php/v2/yb/r/zpNMvPYcpG3.png"))
+                        .setContentDescription("test입니다")
+                        .build();
+                shareDialog.show(content);//공유 이미지 함수를 호출 합니다.
+
+
+
+            }
+        });
+
+        Button btn = (Button) findViewById(R.id.button3); //xml 버튼을 찾습니다.
+        //버튼 클릭 메소드를 생성 합니다.
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+               ImageView image =(ImageView)findViewById(R.id.imageview1);
+                //image.setImageResource(R.drawable.imgsam);
+                BitmapDrawable d = (BitmapDrawable)((ImageView) findViewById(R.id.imageview1)).getDrawable();
+                Bitmap b = d.getBitmap();
+
+                //사진 공유
+               // Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.imgSam);
+
+
+               /* SharePhoto photo = new SharePhoto.Builder()
+                        .setBitmap(b)
+                        .setCaption("Give me my codez or I will ... you know, do that thing you don't like!")
+                        .build();
+
+                SharePhotoContent content = new SharePhotoContent.Builder()
+                        .addPhoto(photo)
+                        .build();
+
+                ShareApi.share(content, null);*/
+
+
+                SharePhoto photo = new SharePhoto.Builder()
+                        .setUserGenerated(true)
+                        .setBitmap(b)
+                        .setCaption("Latest score 하하하하하하")
+                        .build();
+                SharePhotoContent content = new SharePhotoContent.Builder().addPhoto(photo)
+                        .build();
+
+                if (shareDialog.canShow(SharePhotoContent.class)){
+                    shareDialog.show(content);
+                }
+                else{
+                    Log.d("Activity", "you cannot share photos :(");
+                }
+
+            }
+        });
+
+
+
+
+        Button btn3 = (Button) findViewById(R.id.button5); //xml 버튼을 찾습니다.
+        btn3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
+
+
+
+}
---
+++ YuJaTest/app/src/main/java/map/test/yuja/example/jaewon/yujatest/SplashActivity.java
@@ -0,0 +1,43 @@
+package map.test.yuja.example.jaewon.yujatest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.FacebookSdk;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 페이스북 SDK 초기화
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.setApplicationId(getResources().getString(R.string.facebook_app_id));
+
+        setContentView(R.layout.activity_splash);
+
+        // 스플래시 처리 핸들러
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 엑세스 토큰 확인 부분 (추가)
+                Boolean isValidAccessTocken = false;
+
+                if (isValidAccessTocken) {
+                    // 엑세스 토큰이 유효 할 경우 메인 액티비티로 이동
+                    startActivity(new Intent(SplashActivity.this, SignInActivity.class));
+                } else {
+                    // 엑세스 토큰이 유효하지 않을 경우 로그인 액티비티로 이동
+                    startActivity(new Intent(SplashActivity.this, BaseActivity.class));
+                }
+
+                // 스플래시 액티비티 종료
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
