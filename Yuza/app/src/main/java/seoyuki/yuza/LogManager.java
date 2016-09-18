223ed2d406a1a88e56895ff32f6548093fe2e6f7
guitarhyo <guitarhyo@gmail.com>
2016-09-18 16:46:48 +0900

    최종 통합
    최종본

---
+++ Yuza/app/src/main/java/seoyuki/yuza/LogManager.java
@@ -0,0 +1,28 @@
+package seoyuki.yuza;
+
+import android.util.Log;
+
+
+public class LogManager
+{
+    public static final boolean _DEBUG = true;
+    
+    private static final String _TAG = "TMAP_OPEN_API";
+    
+ 
+    public static void printLog(String text)
+    {
+    	if ( _DEBUG )
+    	{
+    		Log.d(_TAG, text);
+    	}
+    }
+    
+    
+    public static void printError(String text)
+    {	
+    	if(_DEBUG)
+    		Log.e(_TAG, "**ERROR** : " + text);
+    }
+    
+}
