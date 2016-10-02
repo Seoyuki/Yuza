ac67b9468082d493aff6a9ba64fc77cbb3f8e79c
Choijaewon123 <ekfkdso12@gmail.com>
2016-10-02 15:20:59 +0900

    검색테스트용입니다
    검색 테스트용입니다

---
+++ Yuza/app/src/main/java/seoyuki/yuza/Student.java
@@ -0,0 +1,35 @@
+package seoyuki.yuza;
+
+/**
+ * Created by jaewon on 2016-10-02.
+ */
+public class Student {
+
+    String name;
+    String address;
+    String content;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
