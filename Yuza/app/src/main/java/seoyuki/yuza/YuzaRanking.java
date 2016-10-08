5715fffa6e2eb967c1ef3fc9b551e6b17129cdaf
guitarhyo <guitarhyo@gmail.com>
2016-10-08 18:26:28 +0900

    sqllite 테스트
    아직 미완성

---
+++ Yuza/app/src/main/java/seoyuki/yuza/YuzaRanking.java
@@ -0,0 +1,14 @@
+package seoyuki.yuza;
+
+
+public class YuzaRanking {
+
+    int _id;
+    int yuza_id;
+    String name;
+    String ret_time;
+    float ret_km;
+
+    public YuzaRanking() {
+    }
+}
