ff551ea6ffeaff6dbfb1130d368eaf49c16902f2
yeojin12345 <bluemeta@gmail.com>
2016-10-09 17:30:46 +0900

    액션바 설정

--- Yuza/app/src/main/java/seoyuki/yuza/SettingActivity.java
+++ Yuza/app/src/main/java/seoyuki/yuza/SettingActivity.java
@@ -1,5 +1,6 @@
 package seoyuki.yuza;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("설정 바꾸기");
+
+
     }
 }
