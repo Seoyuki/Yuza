51c29326af8f641958a7b83e15f128c8740b1f6d
Yeojin <bluemeta@gmail.com>
2016-10-11 06:08:30 +0900

    설정화면 리스트뷰 구현을 위한 준비

--- Yuza/app/src/main/java/seoyuki/yuza/SettingActivity.java
+++ Yuza/app/src/main/java/seoyuki/yuza/SettingActivity.java
@@ -14,6 +14,16 @@ public class SettingActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("설정 바꾸기");
 
+//리스트뷰 참고 : http://recipes4dev.tistory.com/43
+//        ListView listview ;
+//        ListViewAdapter adapter;
+//
+//        // Adapter 생성
+//        adapter = new ListViewAdapter() ;
+//
+//        // 리스트뷰 참조 및 Adapter달기
+//        listview = (ListView) findViewById(R.id.listview1);
+//        listview.setAdapter(adapter);
 
     }
 }
