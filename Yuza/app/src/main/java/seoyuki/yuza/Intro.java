2612ce7e2deeac56439b9f15e096a372699a84eb
guitarhyo <guitarhyo@gmail.com>
2016-10-30 17:45:32 +0900

    인트로 gps설정이동 추ㅜ가
    인트로 gps설정이동 추ㅜ가

--- Yuza/app/src/main/java/seoyuki/yuza/Intro.java
+++ Yuza/app/src/main/java/seoyuki/yuza/Intro.java
@@ -9,11 +9,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 
 public class Intro extends AppCompatActivity {
-
+    AlertDialog aDialog = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +35,54 @@ public class Intro extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
+    }
+
+    //GPS 설정 체크
+    private boolean chkGpsService() {
+
+        String gps = android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+        if (!(gps.matches(".*gps.*") && gps.matches(".*network.*"))) {
+
+            // GPS OFF 일때 Dialog 표시
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setTitle("위치 서비스 설정");
+            builder.setMessage("무선 네트워크 사용, GPS 위성 사용을 모두 체크하셔야 정확한 위치 서비스가 가능합니다.\n위치 서비스 기능을 설정하시겠습니까?");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    // GPS설정 화면으로 이동
+                    Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    intent.addCategory(Intent.CATEGORY_DEFAULT);
+                    startActivity(intent);
+
+                    return;
+                }
+            }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                            System.runFinalizersOnExit(true);
+                            System.exit(0);
+                            return;
+                        }
+                    });
+
+            Log.d("yuza","gsDialog.create()");
+            aDialog = builder.create();
+            aDialog.show();
+            return false;
+
+        } else {
+            return true;
+        }
+    }
+    public void onStart() {
+        super.onStart();
+        if(aDialog != null && aDialog.isShowing()){
+            aDialog.cancel();
+        }
+        final SharedPreferences checkFirstInfoEnd = getSharedPreferences("checkFirstInfoEnd", MODE_PRIVATE);
         if(chkGpsService()){
             // 2초 후 인트로 제거
             Handler handler = new Handler();
@@ -46,56 +95,34 @@ public class Intro extends AppCompatActivity {
                     boolean isFirstInfoEnd = checkFirstInfoEnd.getBoolean("isFirstInfoEnd", false);
 
                     if (isFirstInfoEnd) {
-                        Intent intent = new Intent(Intro.this, TestBtnActivity.class);
+                        Intent intent = new Intent(Intro.this, MainActivity.class);
                         startActivity(intent);
+
                     } else {
                         Intent intent = new Intent(Intro.this, FirstInfoActivity.class);
                         startActivity(intent);
                         SharedPreferences.Editor editor = checkFirstInfoEnd.edit();
                         editor.putBoolean("isFirstInfoEnd", true);
                         editor.commit();
+
                     }
 
                     finish(); // 인트로 종료
                 }
-            }, 2000);
+            }, 1000);
         }
-
 
 
     }
-
-    //GPS 설정 체크
-    private boolean chkGpsService() {
-
-        String gps = android.provider.Settings.Secure.getString(getContentResolver(), android.provider.Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
-
-        if (!(gps.matches(".*gps.*") && gps.matches(".*network.*"))) {
-
-            // GPS OFF 일때 Dialog 표시
-            AlertDialog.Builder gsDialog = new AlertDialog.Builder(this);
-            gsDialog.setTitle("위치 서비스 설정");
-            gsDialog.setMessage("무선 네트워크 사용, GPS 위성 사용을 모두 체크하셔야 정확한 위치 서비스가 가능합니다.\n위치 서비스 기능을 설정하시겠습니까?");
-            gsDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    // GPS설정 화면으로 이동
-                    Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    intent.addCategory(Intent.CATEGORY_DEFAULT);
-                    startActivity(intent);
-                }
-            })
-                    .setNegativeButton("NO", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            finish();
-                            System.runFinalizersOnExit(true);
-                            System.exit(0);
-                            return;
-                        }
-                    }).create().show();
-            return false;
-
-        } else {
-            return true;
+    public void onPause() {
+        super.onPause();
+        if(aDialog != null && aDialog.isShowing()){
+            aDialog.cancel();
         }
+
+    }
+
+    public void onResume() {
+        super.onResume();
     }
 }
